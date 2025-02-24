09a9721857d93b23255b0b03e6ef74791f90d4e6
mkrtchianart <mkrtchianart@gmail.com>
2025-02-24 14:29:43 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import "./globals.css";
 import { WebAppProvider } from "@/components/providers/webapp-provider";
 import { SelectionPrevention } from "./components/SelectionPrevention";
-import { metadata } from "./metadata";
+import { metadata, viewport } from "./metadata";
 
-export { metadata };
-
+export { metadata, viewport };
 
 export default function RootLayout({
   children,
@@ -14,9 +13,6 @@ export default function RootLayout({
 {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </head>
       <body className="pt-tg-header-height pb-tg-nav-height bg-green-700" >
         <SelectionPrevention />
         <WebAppProvider>

192581aca3e46def1f5c94f36d0ebe034846bfc9
mkrtchianart <mkrtchianart@gmail.com>
2025-02-24 14:29:32 +0400

    Update metadata.ts

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,4 +1,11 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export const metadata: Metadata = {
   title: "OIIA",
